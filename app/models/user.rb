9fd8933b14e9b509e5d3cac5e0b8b417f7b0e6cf
briri <brian.riley@ucop.edu>
2020-07-20 15:16:55 -0700

    tried adding devide extension to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@
 
 # Respresents a user of the API
 class User < ApplicationRecord
+
+  extend Devise::Models
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,

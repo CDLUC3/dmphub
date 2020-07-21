5f35fed358c7340b9b30ca58aec94ad88a5fb422
briri <brian.riley@ucop.edu>
2020-07-21 11:14:00 -0700

    fixed puma sstop

--- config/deploy.rb
+++ config/deploy.rb
@@ -61,7 +61,7 @@ namespace :puma do
   desc 'Start Puma'
   task :start do
     on roles(:app), wait: 1 do
-      execute "cd #{release_path} && bundle exec puma -d -e #{fetch(:rails_env)}"
+      execute "cd #{release_path} && puma -d -e #{fetch(:rails_env)}"
     end
   end
 

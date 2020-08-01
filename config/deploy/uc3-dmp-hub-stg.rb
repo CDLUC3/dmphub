8a262e0e8dacf35ff8d81a1e776d1fc66120d6e9
Ashley Gould <agould@ucop.edu>
2020-08-01 13:38:23 -0700

    add role-based cap environment file

---
+++ config/deploy/uc3-dmp-hub-stg.rb
@@ -0,0 +1,4 @@
+set :application, 'DMPHub_Stage_x2'
+#server 'uc3-dmphub02x2-stg.cdlib.org', user: 'dmp', roles: %w[web app db]
+server 'localhost', user: 'dmp', roles: %w[web app db]
+set :rails_env, 'stage'

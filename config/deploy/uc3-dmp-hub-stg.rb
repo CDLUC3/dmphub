ca8b55dc5f3e4fc69936a0e1bf868fb894a2054d
Ashley Gould <agould@ucop.edu>
2020-08-01 15:29:48 -0700

    experimenting with cap environment file vars

--- config/deploy/uc3-dmp-hub-stg.rb
+++ config/deploy/uc3-dmp-hub-stg.rb
@@ -2,3 +2,6 @@ set :application, 'DMPHub_Stage_x2'
 #server 'uc3-dmphub02x2-stg.cdlib.org', user: 'dmp', roles: %w[web app db]
 server 'localhost', user: 'dmp', roles: %w[web app db]
 set :rails_env, 'stage'
+set :repo_url, 'https://github.com/ashleygould/dmphub.git'
+set :branch, 'puppet_integration'
+set :revision, '0.0.0dev9'

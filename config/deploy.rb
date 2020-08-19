f1edf52d490358988a07d191587f4a8c0eac670a
briri <brian.riley@ucop.edu>
2020-08-19 13:22:36 -0700

    playing with timing of aws ssm credentials fetch

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,7 +50,7 @@ set :default_env, { path: '$PATH' }
 
 set :puma_pid, "#{fetch(:capistrano_dir)}/shared/tmp/pids/server.pid"
 
-before :deploy, 'aws_ssm:env_setup'
+after 'git:create_release', 'aws_ssm:env_setup'
 
 after :deploy, 'puma:restart'
 

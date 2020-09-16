249ea541c9f068c4a155f6c87f7b5da76df9c786
briri <brian.riley@ucop.edu>
2020-09-16 09:53:16 -0700

    cleaned out all of the old cap deploy files

--- config/deploy/uc3-dmp-hub-stg.rb
+++
@@ -1,5 +0,0 @@
-set :application, 'DMPHub_Stage_x2'
-server 'localhost', user: 'dmp', roles: %w[web app db]
-set :rails_env, ENV['RAILS_ENV'] || 'production'
-set :branch, ENV['BRANCH'] || 'main'
-set :repo_url, ENV['REPO'] || 'https://github.com/cdluc3/dmphub.git'
--- config/deploy/uc3_dmphub02x2.rb
+++
@@ -1,3 +0,0 @@
-set :application, 'DMPHub_Stage_x2'
-server 'uc3-dmphub02x2-stg.cdlib.org', user: 'dmp', roles: %w[web app db]
-set :rails_env, 'stage'

a1c71df88ba16a5ae72edbfd370346d7fbd7a74e
briri <brian.riley@ucop.edu>
2020-10-28 13:02:36 -0700

    disabled resque. not needed yet

--- config/environments/stage.rb
+++ config/environments/stage.rb
@@ -63,8 +63,8 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
-  config.active_job.queue_adapter     = :resque
-  config.active_job.queue_name_prefix = 'dmphub_stage'
+  # config.active_job.queue_adapter     = :resque
+  # config.active_job.queue_name_prefix = 'dmphub_stage'
 
   config.action_mailer.perform_caching = false
 

dd537a3da626c3c48fbcf3b593392a86395c6026
briri <brian.riley@ucop.edu>
2023-05-19 08:01:54 -0700

    update test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -31,7 +31,7 @@ FactoryBot.define do
   factory :user do
     first_name          { Faker::Name.first_name }
     last_name           { Faker::Name.last_name }
-    email               { Faker::Internet.unique.safe_email }
+    email               { Faker::Internet.unique.email }
     password            { 'password' }
     accept_terms        { true }
     role                { 'user' }
